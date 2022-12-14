d1453f119ba5c1bb37c1699b16f696bb40a9a212
spencerwilson123321 <spencerwilson123321@gmail.com>
2022-12-13 16:09:16 -0800

    Started to fill in the QUICPacketBuilder class with some code.

--- QUIC/QUICPacketBuilder.py
+++ QUIC/QUICPacketBuilder.py
@@ -5,7 +5,18 @@
     used in the QUICSocket module.
 """
 
+from QUICPacket import *
+
 class QUICPacketBuilder:
+    """
+        The QUICPacketBuilder class will contain all of the necessary logic
+        for creating packets which are correct and meet the contraints
+        of each packet format i.e. error checking such as if a user tries
+        to put a value of 300 in a 4 bit field and so on.
+    """
 
     def __init__(self):
+        pass
+
+    def create_initial_packet(self):
         pass
