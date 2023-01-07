d68211bf5754970360171336b3587d80f641432e
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-01-07 12:39:08 -0800

    testing out features

--- client_example.py
+++ client_example.py
@@ -1,12 +1,18 @@
-from QUIC import QUICSocket
+from QUIC.QUICPacket import *
 
 
 if __name__ == "__main__":
-    addr = ("10.0.0.159", 8001)
-    sock = QUICSocket()
-    sock.connect(addr)
-    data, addr = sock.recv(1024)
-    print(data)
-    print(addr)
-    sock.send(b"response")
-    sock.close()
+
+    hdr = LongHeader(type="initial")
+    frames = []
+    pkt = Packet(header=hdr, frames=frames)
+    print(hdr)
+    
+    # addr = ("10.0.0.159", 8001)
+    # sock = QUICSocket()
+    # sock.connect(addr)
+    # data, addr = sock.recv(1024)
+    # print(data)
+    # print(addr)
+    # sock.send(b"response", 1)
+    # sock.close()
