33f6d35b93725bd0d37b6cf8694694a1828b7e96
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-01-08 13:52:41 -0800

    testing out stream frames

--- client_example.py
+++ client_example.py
@@ -9,8 +9,13 @@ if __name__ == "__main__":
     # unpacked = struct.unpack("!BBBLBLBLI", hdr.raw())
     # print(unpacked)
 
-    hdr = ShortHeader(destination_connection_id=4294967295, packet_number=128397)
-    print(hdr)
-    print(hdr.raw())
-    unpacked = struct.unpack("!BLL", hdr.raw())
-    print(unpacked)
+    # hdr = ShortHeader(destination_connection_id=4294967295, packet_number=128397)
+    # print(hdr)
+    # print(hdr.raw())
+    # unpacked = struct.unpack("!BLL", hdr.raw())
+    # print(unpacked)
+
+    frame = StreamFrame(stream_id=1, offset=1182123, length=10, data=b"HelloWorld")
+    print(frame)
+    print(frame.raw())
+    
