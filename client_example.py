e00fc6bd131f04e45324971d4e8fd80fa91b278d
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-01-08 11:42:28 -0800

    Testing out short header

--- client_example.py
+++ client_example.py
@@ -3,9 +3,14 @@ from struct import unpack
 
 if __name__ == "__main__":
 
-    hdr = LongHeader(type="initial", destination_connection_id=4294967295, source_connection_id=19823712, packet_number=128397)
+    # hdr = LongHeader(type="initial", destination_connection_id=4294967295, source_connection_id=19823712, packet_number=128397)
+    # print(hdr)
+    # print(hdr.raw())
+    # unpacked = struct.unpack("!BBBLBLBLI", hdr.raw())
+    # print(unpacked)
+
+    hdr = ShortHeader(destination_connection_id=4294967295, packet_number=128397)
     print(hdr)
     print(hdr.raw())
-    unpacked = struct.unpack("!BBBLBLBLI", hdr.raw())
+    unpacked = struct.unpack("!BLL", hdr.raw())
     print(unpacked)
-    
