1a94ad6bdde82cc78d5993d469af9b2b4c8e68bd
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-01-29 18:32:49 -0800

    asdasd

--- QUIC/QUICNetworkController.py
+++ QUIC/QUICNetworkController.py
@@ -768,6 +768,7 @@ class QUICSenderSideController:
         # Send packets based on the internal congestion control state.
         udp_socket.sendto(packet.raw(), connection_context.get_peer_address())
         self.bytes_in_flight += len(packet.raw())
+        print(f"inside send_packet_cc: {packet.header.packet_number}")
         self.packets_sent[packet.header.packet_number] = PacketSentInfo(time_sent=time(), 
                                                                     in_flight=True,
                                                                     ack_eliciting=True,
@@ -781,6 +782,7 @@ class QUICSenderSideController:
     def send_non_ack_eliciting_packet(self, packet: Packet, udp_socket: socket, connection_context: ConnectionContext) -> None:
         # For non-ack eliciting packets we don't care about congestion control state.
         udp_socket.sendto(packet.raw(), connection_context.get_peer_address())
+        print(f"inside send_non_ack_eliciting_packet: {packet.header.packet_number}")
         self.packets_sent[packet.header.packet_number] = PacketSentInfo(time_sent=time(), 
                                                                     in_flight=False,
                                                                     ack_eliciting=False,
