0b5fe69f8af642e787da386f52c2f4da00dc0d54
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-02-10 15:25:59 -0800

    Added listen() call in tests

--- test1_server.py
+++ test1_server.py
@@ -8,5 +8,6 @@ from QUIC import QUICSocket
 
 if __name__ == "__main__":
     server = QUICSocket(local_ip="10.0.0.131")
+    server.listen(8000)
     client = server.accept() # Accept a connection.
     client.close()           # Close the connection i.e. send ConnectionClose frame.
