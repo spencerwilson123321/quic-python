230c111c7c93d8dbaac1dc7410a4df6bd8be0002
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-02-12 11:22:16 -0800

    Added to if check so that the code runs if the peer has issued a connection close

--- QUIC/QUICNetworkController.py
+++ QUIC/QUICNetworkController.py
@@ -541,7 +541,7 @@ class QUICNetworkController:
             # First, we need to receive all new packets in kernel queue,
             # and process each one.
             packets = self.receive_new_packets(udp_socket)
-            if not packets:
+            if not packets and not self.peer_issued_connection_closed:
                 continue
             self.process_packets(packets, udp_socket)
             if self.peer_issued_connection_closed:
