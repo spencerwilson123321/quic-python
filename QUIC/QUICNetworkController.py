4286c36faa865af2afd0b746d13c5e53ccbe08b4
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-02-12 11:20:34 -0800

    revert to original

--- QUIC/QUICNetworkController.py
+++ QUIC/QUICNetworkController.py
@@ -543,9 +543,9 @@ class QUICNetworkController:
             packets = self.receive_new_packets(udp_socket)
             if not packets:
                 continue
+            self.process_packets(packets, udp_socket)
             if self.peer_issued_connection_closed:
                 return b""
-            self.process_packets(packets, udp_socket)
             data += self._receive_streams[stream_id].read(num_bytes)
             if data:
                 data_not_read = False
