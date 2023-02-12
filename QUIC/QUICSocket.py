1c90746854271b8a6bc80af365b8b463c1a6a079
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-02-11 17:46:27 -0800

    asd

--- QUIC/QUICSocket.py
+++ QUIC/QUICSocket.py
@@ -30,6 +30,7 @@ class QUICSocket:
         # We just need  to copy it's QUICPacketizer and ConnectionContext into a new socket and then return it.
         connection = QUICSocket("")
         # connection._network_controller = self._network_controller
+        connection._network_controller._connection_context = self._network_controller._connection_context
         connection._socket.bind(connection._network_controller._connection_context.get_local_address())
         connection._socket.connect(connection._network_controller._connection_context.get_peer_address())
         connection._network_controller.set_state(CONNECTED)
