154ae7ce6774203f27c39217dd011b28c8e841d9
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-02-18 17:18:48 -0800

    Implemented shutdown function

--- chat_server.py
+++ chat_server.py
@@ -1,18 +1,22 @@
 from QUIC import QUICSocket
 from threading import Lock, Thread, get_ident
-
+import time
 
 class ChatServer:
+
+    SHUTDOWN = False
 
     def __init__(self, ip: str, port: int):
         self.listener = QUICSocket(ip)
         self.listener.listen(port)
+        self.threads: list[Thread] = []
 
 
     def client_thread_handler(self, client: QUICSocket):
-        print(f"Thread {get_ident()} started...")
-        client.close()
-        print(f"Thread {get_ident()} finished...")
+        disconnected = False
+        while not disconnected and not self.SHUTDOWN:
+            time.sleep(2)
+        client.release()
 
 
     def mainloop(self):
@@ -24,11 +28,18 @@ class ChatServer:
             print("Connection accepted...")
             client_thread = Thread(target=self.client_thread_handler, args=(client,))
             client_thread.start()
+            print(f"Thread {client_thread.ident} started...")
+            self.threads.append(client_thread)
 
 
     def shutdown(self):
-        print("\nShutting down server...")
-
+        self.SHUTDOWN = True
+        print("\nShutting down threads...")
+        for thread in self.threads:
+            thread.join()
+            print(f"Thread {thread.ident} finished...")
+        print("Shutting down server...")
+        self.listener.release()
 
 
 # Start with a basic multi-threaded server.
