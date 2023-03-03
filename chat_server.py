ca3866710cf6bb769e91d6e0ef09713157c08620
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-03-02 16:42:02 -0800

    Chat server is complete!

--- chat_server.py
+++ chat_server.py
@@ -13,14 +13,12 @@ class ChatServer:
     def __init__(self, ip: str, port: int):
         self.listener = QUICSocket(ip)
         self.listener.listen(port)
-        self.id = 0
         self.clients = {}
         self.threads: list[Thread] = []
         self.database = Database()
         self.poller = poll()
         self.db_lock = Lock()
         self.client_lock = Lock()
-        self.poller_lock = Lock()
     
     
     def create_account(self, username: str, password: str) -> bool:
@@ -32,78 +30,6 @@ class ChatServer:
         result: bool = self.database.exists(username, password)
         return result
 
-
-    def create_new_client_id(self):
-        self.id += 1
-        return self.id
-
-
-    def client_thread_handler(self, client: QUICSocket, client_id: int):
-        pass
-        # # 1. Get the reason for connection.
-        # reason = b""
-        # username = b""
-        # password = b""
-
-        # while not reason:
-        #     reason, status = client.recv(1, 12)
-
-        # while not username:
-        #     username, status = client.recv(1, 12)
-
-        # while not password:
-        #     password, status = client.recv(1, 12)
-        
-        # reason = reason.strip()
-        # username = username.strip()
-        # password = password.strip()
-
-        # reason = reason.decode("utf-8")
-        # username = username.decode("utf-8")
-        # password = password.decode("utf-8")
-
-        # if reason == "create":
-        #     self.db_lock.acquire()
-        #     result: bool = self.create_account(username, password)
-        #     self.db_lock.release()
-        #     if result:
-        #         client.send(1, b"success")
-        #     else:
-        #         client.send(1, b"fail")
-
-        # if reason == "sign in":
-        #     self.db_lock.acquire()
-        #     result: bool = self.sign_in(username, password)
-        #     self.db_lock.release()
-        #     if result:
-        #         client.send(1, b"success")
-        #         while not status:
-        #             data, status = client.recv(1, 1024)
-        #             if data:
-        #                 self.client_lock.acquire()
-        #                 for id in self.clients:
-        #                     if id == client_id:
-        #                         continue
-        #                     data = username.encode("utf-8") + b": " + data
-        #                     self.clients[id].send(1, data)
-        #                 self.client_lock.release()
-        #     else:
-        #         client.send(1, b"fail")
-        
-        # if status == True:
-        #     client.release()
-        #     self.client_lock.acquire()
-        #     self.clients.pop(client_id)
-        #     self.client_lock.release()
-        #     print("Closing thread.")   
-        #     return
-
-        # while status == False:
-        #     data, status = client.recv(1, 1024)
-        # self.client_lock.acquire()
-        # self.clients.pop(client_id)
-        # self.client_lock.release()
-        # print("Closing thread.")
 
     def epoll_thread_handler(self):
         while not self.SHUTDOWN:
@@ -133,16 +59,6 @@ class ChatServer:
     def mainloop(self):
         """
         """
-        # print("Starting mainloop...")
-        # while True:
-        #     client = self.listener.accept()
-        #     print("Connection accepted...")
-        #     client_id = self.create_new_client_id()
-        #     self.clients[client_id] = client
-        #     client_thread = Thread(target=self.client_thread_handler, args=(client, client_id,))
-        #     client_thread.start()
-        #     print(f"Thread {client_thread.native_id} started...")
-        #     self.threads.append(client_thread)
         print("Starting epoll thread... (this handles broadcasting client messages.)")
         self.epoll_thread = Thread(target=self.epoll_thread_handler)
         self.epoll_thread.start()
@@ -193,20 +109,8 @@ class ChatServer:
                     self.clients[client._socket.fileno()] = (client, username)
                     self.poller.register(client._socket.fileno())
                     self.client_lock.release()
-                    # while not status:
-                    #     data, status = client.recv(1, 1024)
-                    #     if data:
-                    #         self.client_lock.acquire()
-                    #         for id in self.clients:
-                    #             if id == client_id:
-                    #                 continue
-                    #             data = username.encode("utf-8") + b": " + data
-                    #             self.clients[id].send(1, data)
-                    #         self.client_lock.release()
                 else:
                     client.send(1, b"fail")
-
-
 
 
     def shutdown(self):
@@ -221,7 +125,6 @@ class ChatServer:
         self.listener.release()
 
 
-# Start with a basic multi-threaded server.
 if __name__ == "__main__":
 
     server = ChatServer("10.0.0.131", 8000)
