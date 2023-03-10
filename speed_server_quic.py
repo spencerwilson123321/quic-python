d7020f6a83dfe164b1cc159b36ddc79cb06c89bc
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-03-09 18:09:58 -0800

    move print

--- speed_server_quic.py
+++ speed_server_quic.py
@@ -12,11 +12,11 @@ if __name__ == "__main__":
     server = QUICSocket(local_ip="10.0.0.131")
     server.listen(8000)
     num = int(argv[1])
+    print(f"Ready to read {num} bytes:")
     client = server.accept()
     disconnected = False
     data = b""
 
-    print(f"Ready to read {num} bytes:")
     while len(data) < num:
         received, disconnected = client.recv(1, 1024)
         data += received
