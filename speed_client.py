ee41d2a2ee502e182713024eef207091bf352206
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-03-09 17:16:37 -0800

    Creating performance test

---
+++ speed_client.py
@@ -0,0 +1,36 @@
+from QUIC import QUICSocket
+from socket import socket, AF_INET, SOCK_STREAM
+from time import perf_counter
+
+"""
+    Measure and compare the speed of a large data transfer
+    using QUIC and using TCP.
+"""
+
+if __name__ == "__main__":
+
+
+    print("--- Testing QUIC Socket ---")
+    client = QUICSocket(local_ip="10.0.0.159")
+    client.connect(address=("10.0.0.131", 8000))
+    data = b""
+
+    with open("data.txt", "rb") as f:
+        data = f.read()[0:800000]
+    
+    quic_start = perf_counter()
+    client.send(1, data)
+    quic_end = perf_counter()
+    
+    print(f"Sent: {data}")
+    print("Closing the connection...")
+    client.close()
+
+
+    print("--- Testing TCP Socket ---")
+    client = socket(AF_INET, SOCK_STREAM)
+    client.connect(("10.0.0.131", 8000))
+    tcp_start = perf_counter()
+    client.sendall(data)
+    tcp_end = perf_counter()
+    client.close()
---
+++ speed_server.py
@@ -0,0 +1,37 @@
+from QUIC import QUICSocket
+from socket import socket, AF_INET, SOCK_STREAM
+from time import perf_counter
+
+"""
+    Test Case #3:
+        1. Accept client connection.
+        2. Receive a somewhat large amount of data from the client.
+        3. Close the connection.
+"""
+
+if __name__ == "__main__":
+    server = QUICSocket(local_ip="10.0.0.131")
+    server.listen(8000)
+    client = server.accept() # Accept a connection.
+    disconnected = False
+    data = b""
+    # while not disconnected:
+    while len(data) < 800000:
+        received, disconnected = client.recv(1, 1024)
+        data += received
+    print(f"Received data: {data}")
+    print("Client closed the connection.")
+    client.release()           # Close the connection i.e. send ConnectionClose frame.
+    print("QUIC done.")
+
+
+    server = socket(AF_INET, SOCK_STREAM)
+    server.bind(("10.0.0.131", 8000))
+    server.listen(5)
+    client = server.accept()
+    data = b""
+    while len(data) < 800000:
+        data += client.recv(4096)
+    client.close()
+    server.close()
+    print("TCP done.")
