57b021c7ea422011fa9f12f8d22de060468ae844
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-03-10 13:21:17 -0800

    Updated init file to reflect removed module

--- QUIC/__init__.py
+++ QUIC/__init__.py
@@ -3,5 +3,4 @@ from .QUICEncryption import *
 from .QUICPacket import *
 from .QUICPacketParser import *
 from .QUICConnection import *
-from .QUICStreamManager import *
 from .QUICNetworkController import *
