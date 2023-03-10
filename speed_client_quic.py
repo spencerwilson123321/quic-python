bb6f6011be2b9b2013b30cfda4a17bd1ef03df75
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-03-10 13:33:52 -0800

    Added arguments to this file.

--- speed_client_quic.py
+++ speed_client_quic.py
@@ -1,6 +1,15 @@
 from QUIC import QUICSocket
 from time import perf_counter
-from sys import argv
+from argparse import ArgumentParser
+
+PARSER = ArgumentParser()
+PARSER.add_argument("local ip", help="Local IPv4 address.")
+PARSER.add_argument("server ip", help="Server IPv4 address.")
+PARSER.add_argument("server port", help="Server port number")
+PARSER.add_argument("n_bytes", help="Number of bytes to send.")
+
+ARGS = PARSER.parse_args()
+
 
 """
     Measure and compare the speed of a large data transfer
@@ -9,12 +18,16 @@ from sys import argv
 
 if __name__ == "__main__":
 
+    local_ip = ARGS.local_ip
+    server_ip = ARGS.server_ip
+    server_port = int(ARGS.server_port)
+    num = int(ARGS.n_bytes)
 
     print("--- Testing QUIC Socket ---")
-    client = QUICSocket(local_ip="10.0.0.159")
-    client.connect(address=("10.0.0.131", 8000))
+    client = QUICSocket(local_ip=local_ip)
+    client.connect(address=(server_ip, server_port))
     data = b""
-    num = int(argv[1])
+
     with open("data.txt", "rb") as f:
         data = f.read()[0:num]
     
