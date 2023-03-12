eea59abf1c124d3f19e950b6edc853ee8a46ded6
spencerwilson123321 <spencerwilson123321@gmail.com>
2023-03-12 11:05:10 -0700

    Modified these scripts to use command line arguments

--- speed_client_tcp.py
+++ speed_client_tcp.py
@@ -1,6 +1,14 @@
 from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from time import perf_counter
-from sys import argv
+from argparse import ArgumentParser
+
+PARSER = ArgumentParser()
+PARSER.add_argument("server_ip", help="Server IPv4 address.")
+PARSER.add_argument("server_port", help="Server port number")
+PARSER.add_argument("n_bytes", help="Number of bytes to send.")
+
+ARGS = PARSER.parse_args()
+
 
 """
     Measure and compare the speed of a large data transfer
@@ -9,16 +17,18 @@ from sys import argv
 
 if __name__ == "__main__":
 
+    server_ip = ARGS.server_ip
+    server_port = int(ARGS.server_port)
+    num = int(ARGS.n_bytes)
 
     data = b""
-    num = int(argv[1])
     with open("data.txt", "rb") as f:
         data = f.read()[0:num]
 
     print("--- Testing TCP Socket ---")
     tcp_client = socket(AF_INET, SOCK_STREAM)
     tcp_client.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    tcp_client.connect(("10.0.0.131", 8005))
+    tcp_client.connect((server_ip, server_port))
     tcp_start = perf_counter()
     tcp_client.sendall(data)
     tcp_end = perf_counter()
--- speed_server_tcp.py
+++ speed_server_tcp.py
@@ -1,5 +1,13 @@
 from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
-from sys import argv
+from argparse import ArgumentParser
+
+PARSER = ArgumentParser()
+PARSER.add_argument("server_ip", help="Server IPv4 address.")
+PARSER.add_argument("server_port", help="Server port number")
+PARSER.add_argument("n_bytes", help="Number of bytes to send.")
+
+ARGS = PARSER.parse_args()
+
 
 """
     Test Case #3:
@@ -10,10 +18,13 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    num = int(argv[1])
+    num = int(ARGS.n_bytes)
+    ip = ARGS.server_ip
+    port = int(ARGS.server_port)
+
     tcp_server = socket(AF_INET, SOCK_STREAM)
     tcp_server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    tcp_server.bind(("10.0.0.131", 8005))
+    tcp_server.bind((ip, port))
     tcp_server.listen()
     print(f"Ready to read {num} bytes:")
     client, address = tcp_server.accept()
